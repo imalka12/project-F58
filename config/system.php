1d58339fc9d2ece1827446f33e52f41f3959f12a
Imalka Wijerathna <piumiwijerathna1997@gmail.com>
2021-08-27 15:13:43 +0530

    initial files home page design added login and registration process added skote html template added

---
+++ config/system.php
@@ -0,0 +1,5 @@
+<?php
+
+return [
+    'developer' => ['name' => 'Imalka Wijerathna', 'email' => 'piumiwijerathna1997@gmail.com'],
+];
---
+++ resources/views/index.blade.php
@@ -0,0 +1,33 @@
+@extends('layouts.master')
+
+@section('title') @lang('translation.Dashboards') @endsection
+
+@section('content')
+
+    @component('components.breadcrumb')
+        @slot('li_1') Dashboards @endslot
+        @slot('title') Dashboard @endslot
+    @endcomponent
+
+    <div class="container">
+        <form>
+        <div class="form-group">
+            <label for="exampleInputEmail1">Your Email</label>
+            <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter your email address here..">
+            <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
+        </div>
+        <div class="form-group">
+            <label for="exampleInputPassword1">Password</label>
+            <input type="password" class="form-control" id="exampleInputPassword1">
+        </div>
+        <div class="form-group form-check">
+            <input type="checkbox" class="form-check-input" id="exampleCheck1">
+            <label class="form-check-label" for="exampleCheck1">Check me out</label>
+        </div>
+        <button type="submit" class="btn btn-primary">Submit</button>
+        </form>
+    </div>
+
+@endsection
+@section('script')
+@endsection
