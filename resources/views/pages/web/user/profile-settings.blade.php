ad1afebe77b39dfc70d55ca330b381475d8c9feb
Imalka Wijerathna <piumiwijerathna1997@gmail.com>
2021-11-18 22:33:42 +0530

    Conflit merge

--- resources/views/pages/web/user/profile-settings.blade.php
+++ resources/views/pages/web/user/profile-settings.blade.php
@@ -1,7 +1,6 @@
 <div class="row">
     <div class="col-lg-12">
         <h2>Settings</h2>
-<<<<<<< HEAD
 
         <div class="card border border-danger mt-5">
             <div class="card-header bg-danger text-white">
@@ -17,12 +16,5 @@
                 </form>
             </div>
         </div>
-=======
-        <form action="{{ route('user.profile.delete', $user->id) }}" 
-            method="POST" class="d-inline" onsubmit="return confirm('Are you sure that you want to delete your profile?')">
-            @csrf
-            <button class="btn btn-danger">Delete My Profile</button>
-        </form>
->>>>>>> 378766ca4f6f1dfa04b1f942ec9f23af899ad376
     </div>
 </div>
